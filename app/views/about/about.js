feb5f02dec70551dcff7068d94c5e2cfafbe4d5f
NickIliev <nikolay.iliev@telerik.com>
2016-10-16 16:44:17 +0300

    test in abot with multiple players

--- app/views/about/about.js
+++ app/views/about/about.js
@@ -1,1 +1,27 @@
+"use strict";
+var label_1 = require("ui/label");
+var youtube = require("nativescript-youtube-player");
+function onLoaded(args) {
+    var page = args.object;
+    var stack = page.getViewById("pageContainer");
+    var playerOne = new youtube.YouTubePlayerView();
+    playerOne.apiKey = "AIzaSyApfrMXAC3SckEBQ_LOrNDA5qUcDAZAevQ";
+    playerOne.videoKey = "dwdws90cxAA";
+    var labelOne = new label_1.Label();
+    labelOne.text = "NASA";
+    var playerTwo = new youtube.YouTubePlayerView();
+    playerTwo.apiKey = "AIzaSyApfrMXAC3SckEBQ_LOrNDA5qUcDAZAevQ";
+    playerTwo.videoKey = "UGPuEDyAsU8";
+    var labelTwo = new label_1.Label();
+    labelTwo.text = "NativeSxcript";
+    var playerThree = new youtube.YouTubePlayerView();
+    playerThree.apiKey = "AIzaSyApfrMXAC3SckEBQ_LOrNDA5qUcDAZAevQ";
+    playerThree.videoKey = "bU1QPtOZQZU";
+    stack.addChild(playerOne);
+    stack.addChild(labelOne);
+    stack.addChild(playerTwo);
+    stack.addChild(labelTwo);
+    stack.addChild(playerThree);
+}
+exports.onLoaded = onLoaded;
 //# sourceMappingURL=about.js.map
