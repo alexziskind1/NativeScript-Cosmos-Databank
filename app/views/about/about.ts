9abbe961386293b44199a2a7565ac08c63e93154
NickIliev <Nikolay.Iliev@telerik.com>
2016-12-29 17:27:01 +0200

    about

--- app/views/about/about.ts
+++ app/views/about/about.ts
@@ -1,33 +1,6 @@
 import { Page } from "ui/page";
-import { StackLayout } from "ui/layouts/stack-layout";
-import { Label } from "ui/label";
 import { EventData } from "data/observable";
-var youtube = require("nativescript-youtube-player");
 
 export function onLoaded(args: EventData) {
     let page = <Page>args.object;
-
-    var stack = <StackLayout>page.getViewById("pageContainer");
-
-    // var playerOne = new youtube.YouTubePlayerView();
-    // playerOne.apiKey = "AIzaSyApfrMXAC3SckEBQ_LOrNDA5qUcDAZAevQ";
-    // playerOne.videoKey = "dwdws90cxAA";
-    // var labelOne = new Label();
-    // labelOne.text = "NASA";
-
-    // var playerTwo = new youtube.YouTubePlayerView();
-    // playerTwo.apiKey = "AIzaSyApfrMXAC3SckEBQ_LOrNDA5qUcDAZAevQ";
-    // playerTwo.videoKey = "UGPuEDyAsU8";
-    // var labelTwo = new Label();
-    // labelTwo.text = "NativeSxcript";
-
-    // var playerThree = new youtube.YouTubePlayerView();
-    // playerThree.apiKey = "AIzaSyApfrMXAC3SckEBQ_LOrNDA5qUcDAZAevQ";
-    // playerThree.videoKey = "bU1QPtOZQZU";
-
-    // stack.addChild(playerOne);
-    // stack.addChild(labelOne);
-    // stack.addChild(playerTwo);
-    // stack.addChild(labelTwo);
-    // stack.addChild(playerThree);
 }
