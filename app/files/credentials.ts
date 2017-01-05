417e9841d2af64d63b41c70172285faad37fbc77
NickIliev <Nikolay.Iliev@telerik.com>
2017-01-05 15:52:43 +0200

    crede

--- app/files/credentials.ts
+++ app/files/credentials.ts
@@ -1,3 +1,5 @@
 export let YOUTUBE_API_KEY = "AIzaSyDNjDLY6-Buz9AGbQKftj7FVA8oq5NzslQ";
+
 export let NASA_API_KEY = "api_key=jXRI5DynwdFVqt950uq6XMwZtlf6w8mSgpTJTcbX";
+
 export let oauthFacebookUrl = "https://cosmos-databank.firebaseapp.com/__/auth/handler";
